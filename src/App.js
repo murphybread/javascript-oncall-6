13e53fa1b8655726d44dc28b1cead2e360dd98f5
murphybread <nargene@gmail.com>
2024-12-12 13:32:19 +0900

    feat: 캘린더 초기화, 각 idx+1이 date정보

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const WEEKS = ['월', '화', '수', '목', '금', '토', '일'];
-
 // 필요시 Validator.js로 추출
 class Validator {
   static format(input) {
@@ -54,7 +51,8 @@ export const InputView = {
       try {
         const requestValue = await Console.readLineAsync(MESSAGES_INPUT['START']);
         Validator.format(requestValue);
-        return requestValue;
+        const [month, nDay] = requestValue.split(',');
+        return [month, nDay];
       } catch (error) {
         Console.print(MESSAGES_ERROR['INPUT']);
       }
@@ -62,15 +60,63 @@ export const InputView = {
   },
 };
 
+const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const WEEKS = ['월', '화', '수', '목', '금', '토', '일'];
+const HOLIDAYS = [
+  [1, 1],
+  [3, 1],
+  [5, 5],
+  [6, 6],
+  [8, 15],
+  [10, 3],
+  [10, 9],
+  [12, 25],
+];
+
+const MONTHS_PAIR = { 1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31 };
 class Calendar {
   constructor(month, nDay) {
     this.currentMonth = month;
     this.startDay = nDay;
+    this.endOfDay = 0;
+    this.calendarInfo = [];
+  }
+
+  initialize() {
+    this.endOfDay = MONTHS_PAIR[this.currentMonth];
+    let currentDay = WEEKS.indexOf(this.startDay);
+
+    for (let day = 1; day <= this.endOfDay; day++) {
+      this.calendarInfo.push({ date: day, days: WEEKS[currentDay % 7], isHoliday: this.checkHoliday(this.currentMonth, day, WEEKS[currentDay % 7]) });
+      currentDay += 1;
+    }
+  }
+  checkHoliday(month, date, day) {
+    if (day === '토' || day === '일') {
+      console.log(`${month} ${date}휴일입니다`);
+      return true;
+    }
+    for (const holiday of HOLIDAYS) {
+      if (holiday[0] === Number(month) && holiday[1] === Number(date)) {
+        console.log(`${month} ${date} 법정 공휴일입니다`);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  getAllProperty() {
+    console.log(this.calendarInfo);
+    console.log(`${this.currentMonth}, ${this.startDay}요일, 월말 ${this.endOfDay}`);
   }
 }
 class App {
   async run() {
-    InputView.readInput();
+    const [month, nDay] = await InputView.readInput();
+    const calendar = new Calendar(month, nDay);
+    calendar.initialize();
+    calendar.getAllProperty();
   }
 }
 
